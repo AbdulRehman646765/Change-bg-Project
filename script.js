82e07c929cb41aafffe34c6d634bf60b94f54b7b
Abdul Rehman <abdulofficial187@gmail.com>
2024-06-23 17:33:45 +0500

    Bulit Project

---
+++ script.js
@@ -0,0 +1,28 @@
+// // background color change function in Js..............................................................
+document.getElementById("orange").onclick = function () {
+  document.body.style.backgroundColor = `orange`;
+};
+document.getElementById("green").onclick = function () {
+  document.body.style.backgroundColor = `green`;
+};
+document.getElementById("blue").onclick = function () {
+  document.body.style.backgroundColor = `blue`;
+};
+document.getElementById("red").onclick = function () {
+  document.body.style.backgroundColor = `red`;
+};
+document.getElementById("teal").onclick = function () {
+  document.body.style.backgroundColor = `teal`;
+};
+document.getElementById("black").onclick = function () {
+  document.body.style.backgroundColor = `black`;
+};
+document.getElementById("purple").onclick = function () {
+  document.body.style.backgroundColor = `purple`;
+};
+document.getElementById("white").onclick = function () {
+  document.body.style.backgroundColor = `white`;
+};
+document.getElementById("gray").onclick = function () {
+  document.body.style.backgroundColor = `gray`;
+};
